ccf75bc82265a425b744897ca13d87bb31b22f17
xupremix <xupremix.me@gmail.com>
2024-02-19 22:48:24 +0100

    save

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::thread::sleep;
-use std::time::Duration;
 use ai_robot::prelude::MlRobot;
 use clap::{Parser, Subcommand};
 use oxag_ai_j::WrapperTrashinatorRobot;
-use GraphicsLib::client::app::App;
-use GraphicsLib::winit::event_loop::EventLoopBuilder;
+use graphics_lib::client::app::App;
+use graphics_lib::winit::event_loop::EventLoopBuilder;
 
 #[derive(Subcommand)]
 enum Mode {
@@ -23,11 +21,13 @@ fn main() {
     let args = Args::parse();
     match args.mode {
         Mode::Giulio => {
-            let mut gl_robot = WrapperTrashinatorRobot::new(20);
+            let mut gl_robot = WrapperTrashinatorRobot::new(10);
             let eventloop = EventLoopBuilder::new().build().unwrap();
             let mut app = App::new(&eventloop, (38, 48), "Trashinator").unwrap();
             app.start(eventloop, move || {
-                gl_robot.ai_process_tick()
+                let ris = gl_robot.ai_process_tick();
+                eprintln!("{}", ris.0);
+                ris
             }).unwrap()
         }
         Mode::Filippo => {
@@ -35,10 +35,13 @@ fn main() {
                 .set_log(false)
                 .load_model("src/exam_model.pt")
                 .set_map("src/exam_map.bin", 14, 10)
-                .build();let eventloop = EventLoopBuilder::new().build().unwrap();
+                .build();
+            let eventloop = EventLoopBuilder::new().build().unwrap();
             let mut app = App::new(&eventloop, (48, 23), "Mlrobot").unwrap();
             app.start(eventloop, move || {
-                lol_robot.step()
+                let ris = lol_robot.step();
+                eprintln!("{}", ris.0);
+                ris
             }).unwrap()
         }
     }
